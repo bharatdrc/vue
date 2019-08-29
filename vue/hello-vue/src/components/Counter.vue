f5f105c6466fc9af628684b2765217e3be721f79
bharatdrc <bharatmca28@gmail.com>
2019-08-30 00:51:26 +0530

    component

---
+++ vue/hello-vue/src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+<p>sxsssasa111 : {{test}} sasc :{{count}}</p>
+<button @click="count +=1">add</button>
+</div>
+</template>
+
+<script>
+
+export default {
+
+  data(){
+  	return {count: 0,test:"dede121"};
+  },
+  methods:{
+  	addCount:function(){
+  		this.count =	this.count+1;
+  	}
+  }
+
+}
+</script>
